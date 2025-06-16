6f0b69bcee0b521fb5c900446e5ee2a6eddceeb8
Hoang, Nhu Sy <hoangns@gmail.com>
2025-06-16 13:59:23 +0700

    update

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
-  enable_dns_hostnames = true
+  enable_dns_hostnames = true # Cho phép các tài nguyên trong VPC (như EC2 instances) nhận hostname DNS công khai hoặc riêng tư
   tags                 = var.tags
 }
 
